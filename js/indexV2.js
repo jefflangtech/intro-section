49dd7f430129557841655f5f820d231ee6e33131
jefflangtech <jeff.m.lang@gmail.com>
2022-05-19 16:41:19 -0700

    Minor updates to menu dropdowns

--- js/indexV2.js
+++ js/indexV2.js
@@ -4,18 +4,32 @@ const nav = document.querySelector('#nav-menu')
 const accordionControl = document.querySelectorAll('.accordion-ctrl')
 const overlay = document.querySelector('#overlay')
 
+// nav.addEventListener('mouseover', () => {
+//     console.log('Hit the nav')
+// })
+
+nav.addEventListener('mouseleave', () => {
+    let timeoutID = setTimeout(() => {
+        closeMenuContents()
+    }, '100')
+    nav.addEventListener('mouseover', () => {
+        clearTimeout(timeoutID)
+    })
+})
+
 const closeMenuContents = function() {
-    let menuContents = document.querySelectorAll('.accordion-content')
-    let menuArrows = document.querySelectorAll('.menu-arrow')
+    const menuContents = document.querySelectorAll('.accordion-content')
+    const menuArrows = document.querySelectorAll('.menu-arrow')
     menuContents.forEach(content => {
         if(content.style.maxHeight) {
             content.style.maxHeight = null
-            content.style.opacity = 0
+            content.style.opacity = null
+            content.style.zIndex = null
         }
     })
     menuArrows.forEach(arrow => {
         if(arrow.style.transform) {
-            arrow.style.transform = 'rotate(0turn)'
+            arrow.style.transform = null
         }
     })
 }
@@ -30,15 +44,15 @@ menuOpen.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
     menuOpen.classList.toggle('removed')
     menuClose.classList.toggle('removed')
-    nav.style.transform = "translateX(100%)"
-    overlay.style.display = "none"
+    nav.style.transform = null
+    overlay.style.display = null
 })
 
 overlay.addEventListener('click', () => {
     menuOpen.classList.toggle('removed')
     menuClose.classList.toggle('removed')
-    nav.style.transform = "translateX(100%)"
-    overlay.style.display = "none"
+    nav.style.transform = null
+    overlay.style.display = null
     closeMenuContents()
 })
 
@@ -48,12 +62,31 @@ accordionControl.forEach(control => {
         let content = this.nextElementSibling
         if(content.style.maxHeight) {
             content.style.maxHeight = null
-            content.style.opacity = 0
-            menuArrow.style.transform = 'rotate(0turn)'
+            content.style.opacity = null
+            menuArrow.style.transform = null
+            // Tests to know which layout is current
+            if(nav.style[0]) {
+                console.log('Using the vertical nav')
+            } else {
+                console.log('Using the horizontal nav')
+            }
         } else {
             content.style.maxHeight = content.scrollHeight + "px"
             content.style.opacity = 1
+            content.style.zIndex = 50
             menuArrow.style.transform = 'rotate(-0.5turn)'
+            console.dir(content)
+            // The code below loops through the accordion elements
+            // and closes any opened ones when the next one is clicked
+            accordionControl.forEach(controlElement => {
+                if(controlElement !== control && controlElement.nextElementSibling.style.maxHeight) {
+                    controlElement.nextElementSibling.style.maxHeight = null
+                    controlElement.nextElementSibling.style.opacity = null
+                    controlElement.nextElementSibling.style.zIndex = null
+                } else {
+                    console.log(`This is the one`)
+                }
+            })
         }
     })
 })
